17cdf47b07e25773edee1315210552469ace4cd7
ChenYuRen <53305571+Chitoseyu@users.noreply.github.com>
2025-01-13 16:54:57 +0800

    修正Bot功能
    增加權限設置及調整計算問題

--- src/commands/ping/index.js
+++ src/commands/ping/index.js
@@ -1,18 +1,20 @@
-import { SlashCommandBuilder } from "discord.js";
+import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
 
 export const command = new SlashCommandBuilder()
   .setName("ping")
-  .setDescription("確認機器人延遲資訊");
+  .setDescription("確認Bot延遲資訊")
+  .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+  .setDMPermission(false);
 
 export const action = async (ctx) => {
   const sent = await ctx.reply({
     content: "正在計算延遲 ......",
-    withResponse: true,
+    fetchReply: true,
   });
 
   const ping_data = sent.createdTimestamp - ctx.createdTimestamp;
 
   await ctx.editReply(
-    `Bot latency: ${ping_data}ms,  API latency: ${ctx.client.ws.ping}ms`
+    `Bot latency: ${ping_data} ms,  API latency: ${ctx.client.ws.ping} ms`
   );
 };
